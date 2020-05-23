cd91d25dd8350dd3d03f09848c6de234db59e9b5
DmitryKachalov <rubyzza.dev@gmail.com>
2020-05-23 22:52:41 +0200

    update

--- 3/date.js
+++ 3/date.js
@@ -1,5 +1,14 @@
-let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-let today  = new Date();
+let input = prompt('Введите дату в формате', 'ММ/ДД/ГГГГ');
+console.log(input)
+const regex = new RegExp("^\\d{2}[/]\\d{2}[/]\\d{4}$");
+let result = regex.test(input);
+console.log(result)
+if (result === false) {
+    console.log(result)
+    alert('дата не верна')
+} else {
+   let result = input.replace(/\//gi, '.');
+   result = result.replace(/(\d{2})\.(\d{2})/, '$2.$1');
+    alert(result)
+}
 
-console.log(today.toLocaleDateString("en-US")); // 5/30/2006
-console.log(today.toLocaleDateString("'ru-RU'", options)); // 30.05.2006
